a1923783a6b6ee654672d10b18f13e1598bb3dd7
Naxo175 <charles4midou@gmail.com>
2025-11-30 19:20:17 +0100

    Old

--- old/game.py
+++ old/game.py
@@ -18,17 +18,29 @@ imgs.blit(img, (20, 0))
 imgs.blit(img, (10, 10))
 
 font = pygame.font.Font(None, size=30)
+sound = pygame.mixer.Sound("sound.mp3")
 
 x = 0
 
 running = True
+moving = False
+
 while running:
     screen.fill((255, 255, 255))
 
-    img.set_alpha(max(0, 255 - x))
     screen.blit(imgs, (x, 30))
 
-    x += 50 * delta_time
+    hitbox = pygame.Rect(x, 30, img.get_width(), img.get_height())
+
+    m_pos = pygame.mouse.get_pos()
+
+    target = pygame.Rect(300, 0, 160, 280)
+    collision = hitbox.colliderect(target)
+    m_collision = target.collidepoint(m_pos)
+    pygame.draw.rect(screen, (255 * collision, 255 * m_collision, 0), target)
+
+    if moving:
+        x += 50 * delta_time
 
     text = font.render("Hello World!", True, (255, 0, 0))
     screen.blit(text, (300, 100))
@@ -36,6 +48,15 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d:
+                moving = True
+            if event.key == pygame.K_f:
+                sound.play()
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_d:
+                moving = False
+
 
     pygame.display.flip()
 
